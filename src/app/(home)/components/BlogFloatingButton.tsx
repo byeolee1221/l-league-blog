abc11d32cca98232fbfb45d8e2a338ab17619e78
byeolee1221 <zicsso1221@gmail.com>
2025-04-15 02:42:00 +0900

    Style: 태블릿화면에서 플로팅버튼 위치 수정

--- src/app/(home)/components/BlogFloatingButton.tsx
+++ src/app/(home)/components/BlogFloatingButton.tsx
@@ -5,7 +5,7 @@ const BlogFloatingButton = () => {
   return (
     <Link
       href="/posts/write"
-      className="fixed right-6 bottom-20 z-30 rounded-full bg-orange-500 p-3 shadow-lg sm:right-8 sm:bottom-8"
+      className="fixed right-6 bottom-20 z-30 rounded-full bg-orange-500 p-3 shadow-lg sm:right-8 sm:bottom-8 md:bottom-20"
       aria-label="글쓰기"
     >
       <Image src="/icons/icon_create.svg" alt="글쓰기" width={36} height={36} />
