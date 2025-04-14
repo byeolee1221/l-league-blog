602883ff11e02b6c6c9284525b02a294253637bc
byeolee1221 <zicsso1221@gmail.com>
2025-04-15 02:40:13 +0900

    Feat: pc화면 chat, profile, rank 페이지 추가

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,6 @@
+const ChatPage = () => {
+  return <div className="mx-auto flex w-full max-w-7xl flex-col space-y-8 px-4 py-28 md:px-6 lg:px-8">ChatPage</div>;
+};
+
+export default ChatPage;
+
---
+++ src/app/rank/page.tsx
@@ -0,0 +1,7 @@
+const RankPage = () => {
+  return <div className="mx-auto flex w-full max-w-7xl flex-col space-y-8 px-4 py-28 md:px-6 lg:px-8">RankPage</div>;
+};
+
+export default RankPage;
+
+
